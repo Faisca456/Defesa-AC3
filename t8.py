bd2163357292269dd11979c2d7c7e4f152aded86
Faisca456 <igor.amorim@aluno.faculdadeimpacta.com.br>
2020-10-09 16:20:57 -0300

    ate 132

--- t8.py
+++ t8.py
@@ -1,13 +1,25 @@
 import os
 from flask import Flask, jsonify, request
-from math import sqrt
 
 app = Flask(__name__)
 
 @app.route('/')
-def primos():
+def primo_number():
+    number = 3
+    inicio = 1
+    prime = "2, "
 
-
+    while inicio != 100:
+        isprime = 0
+        for i in range(2, number):
+            if number % i == 0:
+                isprime = 1
+                break
+        if isprime:
+            prime = prime + str(number) + ", "
+            inicio += 1
+        number += 1
+    return prime
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
